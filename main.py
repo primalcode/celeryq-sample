21e0ad3fa10a2ee7485227bd92f996b5698ad33a
yoshima <yoshima.fukuda@codeberry.co.jp>
2024-01-20 13:32:52 +0900

    サンプルコード

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from celery import Celery
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+# RabbitMQをバックエンドとして使用する場合
+celery = Celery("celery_sample", broker='amqp://guest:guest@rabbitmq//')
+
+
+@celery.task
+def add(x, y):
+    return x + y
+
+
+@app.get("/add/{x}/{y}")
+async def execute_add(x: int, y: int):
+    task = add.delay(x, y)
+    return {"task_id": task.id}
